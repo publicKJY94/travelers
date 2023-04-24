a4d3c689db3af6ec9b46ce75b1c0d9864ca87e09
koreamax <koreamax012@gmail.com>
2023-04-24 19:03:58 +0900

    허허:

--- project-travelers/src/com/app/services/admin/AdminFrontController.java
+++ project-travelers/src/com/app/services/admin/AdminFrontController.java
@@ -16,16 +16,12 @@ public class AdminFrontController extends HttpServlet{
 		Result result = null;
 		String target = req.getRequestURI().replace(req.getContextPath() + "/", "").split("\\.")[0];
 		
-		System.out.println(target);
-		System.out.println(req.getRequestURI());
-		System.out.println(req.getContextPath());
-		
-		resp.getWriter().print("바보");
-		
 		//target에 따른 if문 분기처리 구간
-	
-		System.out.println("허은상바보");
+		if(target.equals("admin.admin")) {
+			
+		}
 		
+		System.out.println("허은상바보");
 		
 		
 		
