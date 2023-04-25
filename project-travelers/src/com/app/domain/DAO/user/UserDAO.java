9e9ec8cf3f549d5968c736d389e36a5eb0e19e77
koreamax <koreamax012@gmail.com>
2023-04-25 16:12:13 +0900

    작업중

--- project-travelers/src/com/app/domain/DAO/user/UserDAO.java
+++ project-travelers/src/com/app/domain/DAO/user/UserDAO.java
@@ -2,6 +2,7 @@ package com.app.domain.DAO.user;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.app.domain.VO.user.UserVO;
 import com.app.mybatis.config.MyBatisConfig;
 
 public class UserDAO {
@@ -9,5 +10,10 @@ public SqlSession sqlSession;
 	
 	public UserDAO() {
 		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+	
+	//회원가입
+	public void insert(UserVO userVO) {
+		sqlSession.insert("user.insert", userVO);
 	}
 }
---
+++ project-travelers/src/com/app/services/user/controller/JoinOkController.java
@@ -0,0 +1,42 @@
+package com.app.services.user.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.app.Action;
+import com.app.Result;
+import com.app.domain.DAO.user.UserDAO;
+import com.app.domain.VO.user.UserVO;
+
+public class JoinOkController implements Action{
+	@Override
+	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		UserDAO userDAO = new UserDAO();
+		UserVO userVO = new UserVO();
+		Result result = new Result();
+		
+		userVO.setIdentification(req.getParameter("identification"));
+		userVO.setPassword(req.getParameter("password"));
+		userVO.setName(req.getParameter("name"));
+		userVO.setEmail(req.getParameter("email"));
+		userVO.setPhone_number(req.getParameter("phoneNumber"));
+		userVO.setNickname(req.getParameter("nickname"));
+		userVO.setBirthday(req.getParameter("birthday"));
+		userVO.setGender(req.getParameter("gender"));
+		userVO.setIntroducingMessage(req.getParameter("introducingMessage"));
+		userVO.setStatus(req.getParameter("status"));
+		userVO.setCreatedDate(req.getParameter("createdDate"));
+		userVO.setUpdatedDate(req.getParameter("updatedDate"));
+		userVO.setRole(req.getParameter("role"));
+		userVO.setProfilImageName(req.getParameter("profilImageName"));
+		userVO.setProfilImageOriginalName(req.getParameter("profilImageOriginalName"));
+		userVO.setProfilImageSize(req.getParameter("profilImageSize"));
+		
+		userDAO.insert(userVO);
+
+		return null;
+	}
+}
